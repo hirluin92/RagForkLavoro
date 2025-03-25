5ff30e8edcdf2973f659006590178c8e554dde7e
Matteo Finistauri <matteo.finistauri@avanade.com>
2025-03-25 17:52:47 +0100

    Added Redis service

---
+++ models/configurations/redis.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RedisSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix='REDIS_')
+
+    host: str
+    password: str
+    port: Optional[int] = 6380
+    expiration_seconds: Optional[int] = 180
